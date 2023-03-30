84845e8d8c73b1fdef84424f0e40d3dfc7e18d78
Adyson Vieira <contato.adyson@gmail.com>
2023-03-30 16:58:20 -0300

    add rules and immer on project

--- rules/rules.js
+++ rules/rules.js
@@ -41,3 +41,62 @@ function render() {
 }
 
 $store.subscribe(render);
+
+
+
+
+// Regra 2 - Imutabilidade ######
+// A função reducer deve sempre retornar um estado novo, quando este for modificado. Nunca modifique o estado diretamente (ele deve ser imutável). O conceito de mutabilidade interfere principalmente em como lidamos com objetos e arrays.
+
+const initialState = {
+  nome: 'Adyson',
+  idade: 31,
+  sobre: {
+    dados: {
+      cor: 'azul'
+    }
+  }
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'MUDAR_NOME':
+      // retorna um objeto novo
+      return { ...state, nome: action.payload };
+    default:
+      return state
+    } 
+  }
+  
+  
+  // Immer #######
+  // O immer é um pacote que nos fornece uma função na qual podemos utilizar todos os métodos que mutam arrays ou objetos, sem se preocupar com a questão de imutabilidade, pois a função do immer será sempre produzir um novo objeto/array. O mesmo faz parte do pacote Redux Toolkit.
+  
+  
+// Com immer
+const obj1 = {nome: 'Adyson', idade: 31};
+
+// cria um objeto novo e modifica o valor de idade.
+const obj2 = immer.produce(obj1, (draft) => {
+  draft.idade = 32
+})
+
+console.log( obj1 === obj2); // false
+
+
+
+
+
+const reducer = immer.produce((state, action) => {
+  switch (action.type) {
+    case 'MUDAR_NOME':
+      state.nome = action.payload
+      break;
+    case 'MUDAR_COR':
+      state.sobre.dados.cor = action.payload
+      break;
+    default:
+      return state
+
+  }
+}, initialState)
